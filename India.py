21fc559cf0bc61cb10e60f7f6643ec105de73a20
neerajlifechange <144465632+neerajlifechange@users.noreply.github.com>
2023-11-28 23:13:47 +0530

    Update India.py

--- India.py
+++ India.py
@@ -5,3 +5,6 @@ subprocess.run(["pip", "install", "playwright"])
 
 # Install Playwright dependencies
 subprocess.run(["playwright", "install"])
+
+# Install getindianname using pip
+subprocess.run(["pip", "install", "getindianname"])
