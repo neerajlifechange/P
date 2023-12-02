a70a5f085dcdbb49dbb5b709bc9fb78e7ca54469
neerajlifechange <144465632+neerajlifechange@users.noreply.github.com>
2023-12-03 03:46:39 +0530

    Add files via upload

---
+++ Second.py
@@ -0,0 +1,7 @@
+import subprocess
+
+# subprocess.run(["git", "clone", "https://github.com/neerajlifechange/P.git"])
+subprocess.run(["git", "clone", "https://github.com/neerajlifechange/P.git"])
+
+# subprocess.run(["python", "P/India.py"])
+subprocess.run(["python", "P/India.py"])
